65d11f380e74905cdc53aab8e892d1c728d1815b
Abhishek Chandra <abhichandra03@gmail.com>
2023-08-10 21:13:56 +0530

    env

---
+++ App.js
@@ -0,0 +1,93 @@
+import "./App.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { StreamChat } from "stream-chat";
+import Cookies from "universal-cookie";
+import { useState } from "react";
+import JoinGame from "./components/JoinGame";
+import { Chat } from "stream-chat-react";
+
+function App() {
+  const api_key = process.env.REACT_APP_API_KEY;
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+  const client = StreamChat.getInstance(api_key);
+  const [isAuth, setIsAuth] = useState(false);
+  const [signUpShown, setsignUpShown] = useState(false);
+  const [loginShown, setLoginShown] = useState(false);
+
+  const handleSignUp = () => {
+    setsignUpShown(true);
+  };
+  const handleLogin = () => {
+    setLoginShown(true);
+  };
+
+  const logOut = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("fullName");
+    cookies.remove("email");
+    cookies.remove("username");
+    cookies.remove("hashedPassword");
+    client.disconnectUser();
+    setIsAuth(false);
+  };
+
+  if (token) {
+    client
+      .connectUser(
+        {
+          id: cookies.get("userId"),
+          name: cookies.get("username"),
+          fullname: cookies.get("fullname"),
+          email: cookies.get("email"),
+          hashedPassword: cookies.get("hashedPassword"),
+        },
+        token
+      )
+      .then((user) => {
+        console.log(user);
+        setIsAuth(true);
+      });
+  }
+
+  return (
+    <div className="App">
+      {isAuth ? (
+        <Chat client={client}>
+          <JoinGame />
+          <button className="logOut" onClick={logOut}>
+            Logout
+          </button>
+        </Chat>
+      ) : (
+        <>
+          {signUpShown ? <SignUp setIsAuth={setIsAuth} /> : ""}
+          {loginShown ? <Login setIsAuth={setIsAuth} /> : ""}
+          {!loginShown && !signUpShown ? (
+            <div className="intro">
+              <h2>Multiplayer</h2>
+              <h1>Tic Tac Toe</h1>
+              <div className="buttons">
+
+              <button className="regiBtn" onClick={handleSignUp}>
+                {" "}
+                Sign Up
+              </button>
+              <button className="logBtn" onClick={handleLogin}>
+                {" "}
+                Login
+              </button>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cors from "cors";
+import { StreamChat } from "stream-chat";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const api_secret =
+  "";
+
+const serverClient = StreamChat.getInstance(process.env.REACT_APP_API_KEY, process.env.REACT_APP_API_SECRET);
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { fullName, username, email, password } = req.body;
+    const userId = uuidv4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createToken(userId);
+    res.json({ token, userId, fullName, email, username, hashedPassword });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const { users } = await serverClient.queryUsers({ name: username });
+
+    if (users.length == 0) return res.json({ message: "Username not found" });
+
+    const token = serverClient.createToken(users[0].id);
+    const passwordMatch = await bcrypt.compare(
+      password,
+      users[0].hashedPassword
+    );
+
+    if (passwordMatch) {
+      res.json({
+        token,
+        firstName: users[0].fullName,
+        email: users[0].email,
+        username,
+        userId: users[0].id,
+      });
+    }
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("server running");
+});
